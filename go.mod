331884a36f54139a7758a0ed8f88ff05533a73dd
Kirill Mironov <k.mironov01@gmail.com>
2020-11-26 20:21:18 +0300

    test server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KirillMironov/go-server
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func signUp(w http.ResponseWriter, r *http.Request) {
+	email := r.URL.Query().Get("email")
+	password := r.URL.Query().Get("password")
+	log.Println(email, password)
+}
+
+func main() {
+	log.Println("Started")
+
+	http.Handle("/", http.FileServer(http.Dir("C:/projects/web-app/web_app/build/web")))
+	http.HandleFunc("/register/", signUp)
+
+	log.Fatal(http.ListenAndServe(":9090", nil))
+}
