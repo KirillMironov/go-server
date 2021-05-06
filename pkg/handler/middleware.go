40c7abd7b752fee6a6c217fdb0df671022efe02b
Kirill Mironov <k.mironov01@gmail.com>
2021-05-07 00:38:07 +0300

    Auth fix

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -19,6 +19,7 @@ func NewEnsureAuth(handlerToWrap AuthenticatedHandler) *EnsureAuth {
 
 func (rh *EnsureAuth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 	token, err := usecase.GetTokenFromCookies(r)
 	if err != nil {
