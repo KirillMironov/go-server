372c8b3d429f07920efe65ece434f1ea93762ff9
Kirill Mironov <k.mironov01@gmail.com>
2021-05-08 15:00:41 +0300

    Auth fix

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -18,7 +18,7 @@ func NewEnsureAuth(handlerToWrap AuthenticatedHandler) *EnsureAuth {
 }
 
 func (rh *EnsureAuth) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "https://flutter-web-app.ml")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, " +
 		"Accept-Encoding, X-CSRF-Token, Authorization")
--- pkg/handler/users_handler.go
+++ pkg/handler/users_handler.go
@@ -10,7 +10,7 @@ import (
 )
 
 func signUp(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "https://flutter-web-app.ml")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, " +
 		"Accept-Encoding, X-CSRF-Token, Authorization")
@@ -60,7 +60,7 @@ func signUp(w http.ResponseWriter, r *http.Request) {
 }
 
 func signIn(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "https://flutter-web-app.ml")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, " +
 		"Accept-Encoding, X-CSRF-Token, Authorization")
